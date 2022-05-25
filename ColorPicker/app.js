a7c99e4d0642477a4c05ed3b7d81257b67005b03
Tanjil-C <106244770+Tanjil-C@users.noreply.github.com>
2022-05-25 12:38:19 +0100

    Add files via upload

---
+++ ColorPicker/app.js
@@ -0,0 +1,15 @@
+const colours = ["green", "red", "blue", "purple", "yellow", "pink"];
+const btn = document.getElementById("btn");
+const colour = document.querySelector(".colour");
+
+btn.addEventListener("click", function (){
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber)
+
+    document.body.style.backgroundColor = colours[randomNumber];
+    colour.textContent = colours[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colours.length);
+}
